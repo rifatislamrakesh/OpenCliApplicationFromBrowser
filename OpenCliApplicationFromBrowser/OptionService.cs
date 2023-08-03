dc0f7715bd4825ae1d1c6295ea689cc610a1bbdf
Rifat <rifatislamrakesh@gmail.com>
2023-08-03 17:16:11 +0600

    Add project files.

---
+++ OpenCliApplicationFromBrowser/OptionService.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OpenCliApplicationFromBrowser
+{
+    public static class OptionService
+    {
+        public static void RunOptions(Options options)
+        {
+            if (options.Open && !string.IsNullOrEmpty(options.Invoice))
+            {
+                Console.WriteLine("Opening application with Invoice: " + ExtractInvoiceId(options.Invoice));
+            }
+            else if (options.Open)
+            {
+                Console.WriteLine("No Invoice found!");
+            }
+            else
+            {
+                Console.WriteLine("No valid commands!");
+            }
+        }
+
+        private static string ExtractInvoiceId(string url)
+        {
+            const string pattern = "invoiceid=([A-Za-z0-9-]+)";
+
+            Match match = Regex.Match(url, pattern);
+
+            if (match.Success)
+            {
+                return match.Groups[1].Value;
+            }
+
+            return "";
+        }
+    }
+}
---
+++ OpenCliApplicationFromBrowser/Program.cs
@@ -0,0 +1,7 @@
+﻿using CommandLine;
+using OpenCliApplicationFromBrowser;
+
+Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(OptionService.RunOptions);
+
+Console.ReadLine();
